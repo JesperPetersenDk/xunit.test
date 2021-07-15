38c21344cca717a31041d902c8bb4b4d14988749
Jesper Petersen <info@ordklar.dk>
2021-07-15 23:45:10 +0200

    add

--- xunit.test/ArrayTest.cs
+++ xunit.test/ArrayTest.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace xunit.test
 {
     class ArrayTest
     {
+        [Theory]
+        [InlineData(3)]
+        public void NumberSuccesTest(int x)
+        {
+            var result = ArrayNumber[x]
+            Assert.NotEqual(10, Add(x, y));
+        }
+
+        int[] ArrayNumber()
+        {
+            int[] result = { 4, 31, 51, -2, -22, 0 };
+            return result;
+        }
+
+        string[] ArrayAll()
+        {
+            string[] result = { "BMW", "Ford", "Ford", "Volvo" };
+            return result;
+        }
+
     }
 }
